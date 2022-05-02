b971e15cd64134c751dcb10b9071236d49e258a1
jwbarkes <jbarkes@ucsd.edu>
2022-05-01 23:25:46 -0700

    fix test

--- skillDemo1tester.java
+++ skillDemo1tester.java
@@ -5,6 +5,6 @@ import org.junit.*;
 public class skillDemo1tester {
     @Test
     public void addition() {
-        assertEquals(2, skillDemo1.add(1, 4));
+        assertEquals(5, skillDemo1.add(1, 4));
     }
 }
